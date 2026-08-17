fcd6040abebbac3674f2dc2323121517cbcd18b9
Ruth-Wang37 <160226360+Ruth-Wang37@users.noreply.github.com>
2026-08-17 12:05:19 +0300

    adding the logic for converting from celcius to farenheit and vise verse in js

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const text = document.getElementById("text");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+
+let temp;
+
+
 function convert(){
-    
+    if(toFahrenheit.checked){
+        temp = Number(text.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";  //toFixed gives us to one degree eg 32.0 instead of 32
+
+    }else if(toCelcius.checked){
+        temp = Number(text.value);
+        temp = (temp - 32 ) *(5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+
+    }else{
+        result.textContent = "Select a unit";
+    }
+
+
 }
